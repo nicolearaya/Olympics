037953d08a1b2879bc1b589eb08dcfdaa6e2a6b0
Marie Konopacki <mariekonopacki@college.harvard.edu>
2020-12-05 18:12:06 -0500

    updates

--- js/popularity.js
+++ js/popularity.js
@@ -15,7 +15,7 @@ class PopularityVis {
         // Aggregate the sport name, popularity ranking, and accessibility score
         vis.popularityData.forEach(sport => {
             if (vis.accessibilityScores[sport.Sport]) {
-                vis.displayData.push([sport.Sport, sport.Popularity, vis.accessibilityScores[sport.Sport].total])
+                vis.displayData.push([sport.Sport, (+sport.Popularity * 10).toFixed(1), (+vis.accessibilityScores[sport.Sport].total).toFixed(2)])
             }
         })
 
@@ -75,10 +75,77 @@ class PopularityVis {
 
         // Create tooltip
         vis.tooltip = d3.select("body").append("div")
-            .attr("class", "gdp-tooltip")
+            .attr("class", "popularity-tooltip")
             .style("opacity", 0)
 
+        // Reformat the data: d3.hexbin() needs a specific format
+        vis.hexData = []
+        vis.displayData.forEach(function(d) {
+            vis.hexData.push({x: +d[2], y: +d[1]} )
+        })
 
+        // Compute the hexbin data
+        var hexbin = d3.hexbin()
+            .radius(25 * vis.width / (vis.height - 1)) // size of the bin in px
+            .x(d => vis.x(d.x))
+            .y(d => vis.y(d.y))
+            .extent([ [0, 0], [vis.width, vis.height] ])
+
+        vis.svg.append("clipPath")
+            .attr("id", "clip")
+            .append("rect")
+            .attr("class", "mesh")
+            .attr("width", vis.width)
+            .attr("height", vis.height);
+
+        vis.svg.append("svg:path")
+            .attr("clip-path", "url(#clip)")
+            .attr("d", hexbin.mesh())
+            .style("stroke-width", 3)
+            .style("stroke", "var(--lighterpurple)")
+            .style("fill", "none");
+
+        vis.color = d3.scaleLinear()
+            .range(["#050224", "#110554"])
+            .domain([0, 6])
+
+        // Plot the hexbins
+        vis.svg.append("clipPath")
+            .attr("id", "clip")
+            .append("rect")
+            .attr("width", vis.width)
+            .attr("height", vis.height)
+
+        vis.svg.append("g")
+            .attr("clip-path", "url(#clip)")
+            .selectAll("path")
+            .data( hexbin(vis.hexData) )
+            .enter().append("path")
+            .attr("d", hexbin.hexagon())
+            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+            .attr("fill", d => vis.color(d.length))
+            .style("stroke-width", 3)
+            .style("stroke", "var(--lighterpurple)")
+
+        // Regression line
+        vis.linearRegression = d3.regressionLinear()
+            .x(d => d.x)
+            .y(d => d.y)
+
+        vis.regressionLine = [vis.linearRegression(vis.hexData)[0], vis.linearRegression(vis.hexData)[1]]
+
+        vis.line = d3.line()
+            .x(d => vis.x(d[0]))
+            .y(d => vis.y(d[1]))
+
+        vis.svg.append('path')
+            .classed('regressionLine', true)
+            .datum(vis.regressionLine)
+            .attr('d', vis.line)
+            .style("stroke-dasharray", ("5, 5"))
+            .attr("stroke", "white");
+
+        // Create dots for sports
         vis.sport = vis.svg.selectAll(".popularity-dot")
             .data(vis.displayData)
 
@@ -86,23 +153,37 @@ class PopularityVis {
             .merge(vis.sport)
             .attr("class", "popularity-dot")
             .on("mouseover", function(e, d) {
+                d3.select(this)
+                    .transition()
+                    .duration(400)
+                    .attr("r", 12)
+                    .attr("stroke", "#5923fc")
+                    .attr("stroke-width", "3")
                 vis.tooltip.transition()
-                    .duration(200)
-                    .style("opacity", .9);
-                vis.tooltip.html(d[0])
-                    .style("left", (e.pageX) + "px")
-                    .style("top", (e.pageY - 28) + "px");
+                    .transition()
+                    .duration(400)
+                    .style("opacity", 1);
+                vis.tooltip
+                    .html(`<h3>${d[0]}</h3><br>Popularity: ${d[1]}<br>Accessibility: ${d[2]}`)
+                    .style("left", (e.pageX + 20) + "px")
+                    .style("top", (e.pageY - 50) + "px");
             })
             .on("mouseout", function(d) {
                 vis.tooltip.transition()
                     .duration(500)
                     .style("opacity", 0);
+                d3.select(this)
+                    .transition()
+                    .duration(400)
+                    .attr("r", 6)
+                    .attr("stroke", "#530e0e")
+                    .attr("stroke-width", "1")
             })
             .attr("cx", d => vis.x(d[2]))
             .attr("cy", d => vis.y(+d[1]))
-            .attr("r", 10)
-            .attr("stroke", "black")
-            .attr("fill", "blue")
+            .attr("stroke", "#530e0e")
+            .attr("r", 6)
+            .attr("fill", "var(--red)")
 
         vis.sport.exit().remove();
     }
